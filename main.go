70c2b1b43c153b07fc9439a9a5b2390642078ac4
Carlos Camilo <carloscamilocontato@gmail.com>
2022-09-06 19:21:03 -0300

    Server setu

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kalogs-c/piadocas/controllers"
+	"github.com/kalogs-c/piadocas/seed"
+)
+
+var server controllers.Server = controllers.Server{}
+
 func main() {
-	println("Hello World")
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, not comming through %v", err)
+	}
+
+	server.Initialize(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	server.Run(":8080")
 }
